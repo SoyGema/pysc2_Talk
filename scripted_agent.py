2ec49345981d133e79d9c7573287c80d93810506
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-11-04 12:48:48 -0700

    Add special thanks user

--- scripted_agent.py
+++ scripted_agent.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Special thanks to : Thebunni
+#                     Alekahpwn
 
 """Scripted agents."""
 
